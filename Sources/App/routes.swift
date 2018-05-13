e4992caf911b6c069185af3ee36e6aaebcb119d0
Frédéric ADDA <frederic.adda@me.com>
2018-05-13 11:00:11 +0200

    Use PostgreSQL as the database

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,6 +3,11 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     
+
+    router.get("hello") { req -> String in
+        return "Hello World"
+    }
+    
     router.post("api", "acronyms") { req -> Future<Acronym> in
         return try req.content.decode(Acronym.self)
             .flatMap(to: Acronym.self, { acronym in
